a4a607aba8b441229ddb3db369f4febb43f04b43
marie-bisbo <49270534+marie-bisbo@users.noreply.github.com>
2024-09-19 08:18:11 +0100

    Add basic state machine

--- character.gd
+++ character.gd
@@ -4,21 +4,56 @@ const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 @onready var animated_sprite = $AnimatedSprite2D
 
+enum state {IDLE, RUNNING, JUMPING, ATTACKING, ROLLING}
+
+var current_state = state.IDLE
+
 func _physics_process(delta):
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-		animated_sprite.play("jump")
-	else:
-		var direction = Input.get_axis("move_left", "move_right")
-		if direction:
-			velocity.x = direction * SPEED
-			animated_sprite.play("run")
-			animated_sprite.flip_h = direction < 0
-		else:
+	set_current_state()
+	
+	match current_state:
+		state.IDLE:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 			animated_sprite.play("idle")
-		
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		state.RUNNING:
+			var direction = Input.get_axis("move_left", "move_right")
+			if direction:
+				velocity.x = direction * SPEED
+				animated_sprite.play("run")
+				animated_sprite.flip_h = direction < 0
+		state.JUMPING:
+			velocity += get_gravity() * delta
+			animated_sprite.play("jump")
+		state.ATTACKING:
+			animated_sprite.play("attack")
+		state.ROLLING:
+			pass
 
 	move_and_slide()
+
+func set_current_state():
+	if Input.is_action_just_pressed("attack"):
+		current_state = state.ATTACKING
+	else:
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		if not is_on_floor():
+			current_state = state.JUMPING
+		else:
+			var direction = Input.get_axis("move_left", "move_right")
+			current_state = state.RUNNING if direction else state.IDLE
+			
+func idle():
+	pass
+	
+func run():
+	pass
+	
+func jump():
+	pass
+	
+func attack():
+	pass
+	
+func roll():
+	pass
