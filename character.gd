b6db47995b824bde615b89bb5de1e13158f94b73
marie-bisbo <49270534+marie-bisbo@users.noreply.github.com>
2024-09-23 11:16:44 +0100

    Add move whilst in air

--- character.gd
+++ character.gd
@@ -21,13 +21,13 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+		animated_sprite.flip_h = direction < 0
 	else:
-		var direction = Input.get_axis("move_left", "move_right")
-		if direction:
-			velocity.x = direction * SPEED
-			animated_sprite.flip_h = direction < 0
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
 
